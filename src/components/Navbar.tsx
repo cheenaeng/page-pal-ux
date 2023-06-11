ffb90cc451b1351aa6631c4d74142c0bd25ed48a
nyangshawbin <shawbin.nyang@circles.asia>
2023-06-11 14:19:16 +0800

    refactor: move color switcher to navBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,10 +9,11 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react'
 import { FiMenu } from 'react-icons/fi'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
 
 const menuMap = new Map<string, string>([
-  ['Saved', 'saved'],
-  ['Archived', 'archived'],
+  ['Saves', 'saves'],
+  ['Archives', 'archives'],
   ['My Stats', 'my_stats']
 ])
 
@@ -27,14 +28,18 @@ export const Navbar = () => {
               <Flex justify='space-between' flex='1'>
                 {/* Menus */}
                 <ButtonGroup variant='text' colorScheme='gray' spacing='8'>
-                  {['Saved', 'Archive', 'My Stats'].map((item) => (
+                  {['Saves', 'Archives', 'My Stats'].map((item) => (
                     <Button key={item}>{item}</Button>
                   ))}
                 </ButtonGroup>
-                {/* sign in/ up */}
-                <HStack spacing='3'>
+
+                {/* Sign in/ up */}
+                <HStack spacing='3' justifySelf='flex-end'>
                   <Button variant='tertiary'>Sign in</Button>
                   <Button variant='primary'>Sign up</Button>
+
+                  {/* Light/dark mode */}
+                  <ColorModeSwitcher />
                 </HStack>
               </Flex>
             ) : (
